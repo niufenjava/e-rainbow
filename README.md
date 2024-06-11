8128243a1ee80b8f312261a847047b50f08400d7
haijun.zhang <haijun.zhang@cotticoffee.com>
2024-06-11 20:33:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+git remote set-url origin https://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@github.com/niufenjava/e-rainbow.git
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const routes = [
+    // 你的路由配置
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+export default router;
